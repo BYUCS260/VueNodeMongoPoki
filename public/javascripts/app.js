4fcde92970ea6ba11ae1ae7cd1d5de3e89f39a05
Mark Clement <clement@cs.byu.edu>
2019-06-26 14:38:15 -0600

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,41 +1,43 @@
-var app = window.angular.module('app', [])
-
-app.factory('pokemonFetcher', pokemonFetcher)
-app.controller('mainCtrl', mainCtrl)
-
-function pokemonFetcher($http) {
-
-  var API_ROOT = 'pokemon'
-  return {
-    get: function() {
-      return $http
-        .get(API_ROOT)
-        .then(function(resp) {
-          return resp.data
-        })
+/*global axios */
+/*global Vue */
+var app = new Vue({
+  el: '#app',
+  data: {
+    pokis: [],
+    pokiName: '',
+    pokiURL: '',
+  },
+  created: function() {
+    this.getpokis();
+  },
+  methods: {
+    async getpokis() {
+      // `this` points to the vm instance
+      console.log("get pokis");
+      var url = "http://yourserver:4200/pokemon";
+      try {
+        let response = await axios.get(url);
+        this.pokis = response.data;
+        console.log(this.pokis);
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
     },
-    post: function(formData) {
-      return $http
-        .post(API_ROOT, formData)
-        .then(function(resp) {
-          console.log("Post worked");
+    addItem() {
+      var url = "http://yourserver:4200/pokemon";
+      axios.post(url, {
+          name: this.pokiName,
+          avatarUrl: this.pokiURL
         })
-    }
+        .then(response => {})
+        .catch(e => {
+          console.log(e);
+        });
+      this.pokiName = '';
+      this.pokiURL = '';
+      this.getpokis();
+    },
   }
-}
-
-function mainCtrl($scope, pokemonFetcher) {
-
-  $scope.pokemon = []
-
-  pokemonFetcher.get()
-    .then(function(data) {
-      $scope.pokemon = data
-    })
-  $scope.addPoki = function() {
-    var formData = { name: $scope.Name, avatarUrl: $scope.Url };
-    console.log(formData);
-    pokemonFetcher.post(formData); // Send the data to the back end
-    $scope.pokemon.push(formData); // Update the model
-  }
-}
+});
